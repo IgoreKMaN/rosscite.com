9aa72b9d099fed594d0fe69bc88e6bf1813f2d99
y1876 <y1876@mail.ru>
2019-01-13 21:06:19 +0300

    First

--- script/script.js
+++ script/script.js
@@ -1,4 +1,16 @@
 $( document ).ready(function() {
     
+	$("button").mousedown(function(){
+  		$("button").css({'background-color':'#e47a0f'});
+	});
+	$("button").mouseup(function(){
+  		$("button").css({'background-color':'#ff891e'});
+	});
+
+	$("button").click(function () {
+		setInterval("$('.main').css({'display':'none'})", 5000)
+		
+  	});
+
 
 });
